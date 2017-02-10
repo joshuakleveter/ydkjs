f7648bea99a3be986ebd023fbc499dabddff0688
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-10 17:03:55 -0600

    Fixed issues in app.js

--- scope_and_closures/app.js
+++ scope_and_closures/app.js
@@ -1,14 +1,14 @@
 /**
  * LHS (left hand side) assignment.
- * 
+ *
  * In LHS we don't care about the current value,
  * we just need the container so we can assign it a value.
  */
-var a = "value";
+let a = 'value';
 
 /**
  * RHS (right hand side) assignment.
- * 
+ *
  * In RHS we need to get the value from the container.
  */
 console.log(a);
